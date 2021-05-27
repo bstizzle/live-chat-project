12a311ada875bbb533c0b49852f0a7a5b187811d
Ben Stein <whereyouben@gmail.com>
2021-05-27 13:27:12 -0400

    removed old commented code

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -5,26 +5,6 @@ import moment from "moment";
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
-
-  
-  // const orderedMessages = []
-  // //thought about using just .reverse(), but that alters the array in place
-  // //so it actually reverses the array every time you go back to the same conversation
-  // for(let i = messages.length-1; i >= 0; i--){
-  //   const message = messages[i]
-  //   const time = moment(message.createdAt).format("h:mm");
-    
-  //   if(message.senderId === userId){
-  //     orderedMessages.push(
-  //       <SenderBubble key={message.id} text={message.text} time={time} />
-  //     )
-  //   } else {
-  //     orderedMessages.push(
-  //       <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
-  //     )
-  //   }
-  // }
-  
   
   return (
     <Box>
