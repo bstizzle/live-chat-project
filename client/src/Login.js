2e91cf0c6976b1c897f81c37725632b939e91a9e
Ben Stein <whereyouben@gmail.com>
2021-05-27 16:20:54 -0400

    login page mostly matches spec and accounts for mobile, formatting the svg/text overlay next

--- client/src/Login.js
+++ client/src/Login.js
@@ -3,38 +3,45 @@ import { Redirect, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
 import {
   Grid,
-  Box,
   Typography,
   Button,
   FormControl,
-  TextField,
-  Paper
+  Paper,
+  Input,
+  InputLabel,
+  Hidden
 } from "@material-ui/core";
 import { login } from "./store/utils/thunkCreators";
 
 import { makeStyles } from "@material-ui/core/styles";
 import bgImage from './images/bg-img.png';
+import bubble from './images/bubble.svg';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
-    alignItems: 'center',
+    height: '100vh'
   },
-  content: {
-    padding: "20px",
-    flexDirection: "column"
+  imgOver: {
+    textAlign: 'center',
+  },
+  input: {
+    padding: "10px"
   },
   header: {
-    display: 'flex',
-    flexDirection: "row"
+    paddingTop: "50px"
+  },
+  headerTxt: {
+    paddingTop: "25px", 
+    color: "#C0C0C0"
   },
   paper: {
-    textAlign: "center",
+    textAlign: "center",  
   },
   button: {
-    boxShadow: "0 2px 20px 0 rgba(88,133,196,0.10)",
+    boxShadow: "0 2px 20px 0 rgba(88,133,196,0.40)",
     width: "200px",
-    height: "100px"
+    height: "80px",
   }
 }))
 
@@ -56,82 +63,60 @@ const Login = (props) => {
   }
 
   return (
-    <>
-    <Box className={classes.root}>
-      <img src={bgImage} alt="sidebar" />
-        <Grid container spacing={3}>
-          <Grid item xs={6}>
-            <Typography>Need to register?</Typography>
-          </Grid>
-          <Grid item xs={6}>
-            <Button className={classes.button} onClick={() => history.push("/register")}>Register</Button>
-          </Grid>
-          <Grid item xs={12} alignContent="center">
-            <Paper className={classes.paper} elevation={0}>
-            <form onSubmit={handleLogin}>
-            <Grid item>
-              <FormControl required>
-                <TextField
-                  aria-label="username"
-                  label="Username"
-                  name="username"
-                  type="text"
-                />
-              </FormControl>
+    <div className={classes.root}>
+      <Hidden smDown>
+        <div className="sidebar" style={{"--img": `url("${bgImage}")`}}>
+          <Grid
+            container
+            justify="center"
+            alignItems="center"
+            className={classes.imgOver}
+          >
+            <Grid item xs={12}>
+              <img src={bubble} alt="bubble" />
             </Grid>
-            <Grid item>
-              <FormControl required>
-                <TextField
-                  aria-label="password"
-                  label="Password"
-                  name="password"
-                  type="text"
-                />
-              </FormControl>
+            <Grid item xs={12}>
+              Converse with anyone<br></br>
+              with any language
+            </Grid>
+          </Grid>
+        </div>
+      </Hidden>
+      <Grid container spacing={3} className={classes.header}>
+        <Grid item sm={8} xs={6}>
+          <Typography className={classes.headerTxt} align="right">Don't have an account?</Typography>
+        </Grid>
+        <Grid item sm={4} xs={6}>
+          <Button className={classes.button} onClick={() => history.push("/register")} style={{color:"#3A8DFF"}}>
+            Create Account
+          </Button>
+        </Grid>
+        <Grid item xs={12} style={{padding: '100px'}}>
+          <Paper className={classes.paper} elevation={0} justify="center">
+            <Typography variant="h4" style={{textAlign: "left", fontWeight: "bold", paddingBottom: '40px'}}>Welcome back!</Typography>
+            <form onSubmit={handleLogin}>
+              <Grid item xs={12}>
+                <FormControl className={classes.input} fullWidth required>
+                  <InputLabel htmlFor="username">Username</InputLabel>
+                  <Input id="username" aria-describedby="username" />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <FormControl className={classes.input} fullWidth required>
+                  <InputLabel htmlFor="password">Password</InputLabel>
+                  <Input id="password" aria-describedby="password" type="password" />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} style={{paddingTop: '40px'}}>
+                <Button className={classes.button} color="primary" type="submit" variant="contained" size="large">
+                  Login
+                </Button>
               </Grid>
             </form>
-            </Paper>
-          </Grid>
+          </Paper>
         </Grid>
-    </Box>
-
-
-    <Grid container justify="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to register?</Typography>
-          <Button onClick={() => history.push("/register")}>Register</Button>
-        </Grid>
-        <form onSubmit={handleLogin}>
-          <Grid>
-            <Grid>
-              <FormControl margin="normal" required>
-                <TextField
-                  aria-label="username"
-                  label="Username"
-                  name="username"
-                  type="text"
-                />
-              </FormControl>
-            </Grid>
-            <FormControl margin="normal" required>
-              <TextField
-                label="password"
-                aria-label="password"
-                type="password"
-                name="password"
-              />
-            </FormControl>
-            <Grid>
-              <Button type="submit" variant="contained" size="large">
-                Login
-              </Button>
-            </Grid>
-          </Grid>
-        </form>
-      </Box>
-    </Grid>
-    </>
+      </Grid>
+    </div>
   );
 };
 
